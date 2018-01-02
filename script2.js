e35060c6d34cd7078bc1e92e5b6543c0356fd5d8
unknown <micbarash@gmail.com>
2018-01-02 15:10:15 +0200

    Added functions to count round winners, game declares a winner who reaches 5 round wins.

--- script2.js
+++ script2.js
@@ -1,3 +1,18 @@
+//Variable declaration
+let playerWins = 0;
+let computerWins = 0;
+const playerScore = document.querySelector('.player');
+const computerScore = document.querySelector('.computer');
+const winner = document.getElementById('winner');
+const header = document.querySelector('h3');
+const rckBtn = document.getElementById('rock');
+const pprBtn = document.getElementById('paper');
+const scsBtn = document.getElementById('scissors');
+//Event listeners
+rckBtn.addEventListener("click", caseRock);
+pprBtn.addEventListener("click", casePpr);
+scsBtn.addEventListener("click", caseScs);
+//Functions
 function computerPlay() {
   let choice = Math.floor(Math.random() * 3);
   if (choice === 0) {
@@ -8,40 +23,63 @@ function computerPlay() {
     return "SCISSORS";
   }
 };
-//const header = document.querySelector('h3');
-const rckBtn = document.getElementById('rock');
-const pprBtn = document.getElementById('paper');
-const scsBtn = document.getElementById('scissors');
-rckBtn.addEventListener("click", caseRock);
-pprBtn.addEventListener("click", casePpr);
-scsBtn.addEventListener("click", caseScs);
+function chooseWinner() {
+  if (playerWins === 5) {
+    winner.innerHTML = 'YOU ARE THE WINNER!!'
+  } else if (computerWins === 5) {
+    winner.innerHTML = 'COMPUTER IS THE WINNER!!'
+  } else {
+    winner.innerHTML = 'NO WINNER YET'
+  }
+  if (playerWins > 5 || computerWins > 5) {
+    alert('Game is over, please refresh the page to play again');
+  }
+}
 function caseRock(computerSelection) {
   computerSelection = computerPlay()
   if(computerSelection === "SCISSORS") {
-    alert('You win! rock beats scissors')
+    header.innerHTML = 'You win! rock beats scissors';
+    playerWins++;
+    playerScore.innerHTML = 'PLAYER SCORE: ' + playerWins;
+    chooseWinner();
   } else if (computerSelection === "PAPER") {
-    alert('You lose! paper beats rock')
+    header.innerHTML = 'You lose! paper beats rock';
+    computerWins++;
+    computerScore.innerHTML = 'COMPUTER SCORE: ' + computerWins;
+    chooseWinner();
   } else {
-    alert('Its a tie!')
+    header.innerHTML = 'Its a tie!';
   }
 };
 function casePpr(computerSelection) {
   computerSelection = computerPlay()
   if(computerSelection === "SCISSORS") {
-    alert('You lose! scissors beat paper')
+    header.innerHTML = 'You lose! scissors beat paper';
+    computerWins++;
+    computerScore.innerHTML = 'COMPUTER SCORE: ' + computerWins;
+    chooseWinner();
   } else if (computerSelection === "ROCK") {
-    alert('You win! Paper beats rock')
+    header.innerHTML = 'You win! Paper beats rock';
+    playerWins++
+    playerScore.innerHTML = 'PLAYER SCORE: ' + playerWins;
+    chooseWinner();
   } else {
-    alert('Its a tie!')
+    header.innerHTML = 'Its a tie!';
   }
 };
 function caseScs(computerSelection) {
   computerSelection = computerPlay()
   if(computerSelection === "ROCK") {
-    alert('You lose! rock beats scissors')
+    header.innerHTML = 'You lose! rock beats scissors';
+    computerWins++
+    computerScore.innerHTML = 'COMPUTER SCORE: ' + computerWins;
+    chooseWinner();
   } else if (computerSelection === "PAPER") {
-    alert('You win! scissors beat paper')
+    header.innerHTML = 'You win! scissors beat paper';
+    playerWins++
+    playerScore.innerHTML = 'PLAYER SCORE: ' + playerWins;
+    chooseWinner();
   } else {
-    alert('Its a tie!')
+    header.innerHTML = 'Its a tie!';
   }
 };
